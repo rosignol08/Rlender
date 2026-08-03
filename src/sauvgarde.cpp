4dba9652919b528340a976dc1d61c6bd2dbf4133
rosignol08 <rchaffray@gmail.com>
2026-08-03 13:52:14 +0200

    pause midi

--- src/sauvgarde.cpp
+++ src/sauvgarde.cpp
@@ -1,3 +1,15 @@
 /*
 ici je defini des fonction pour les sauvgardes pour aleger le code dans le main
 */
+#include <iostream>
+#include <fstream>
+
+/*
+fonction utilitaire pour enregistrer un flux de char dans un fichier sur le disque dur
+on a besoin du nom du fichier à sauvgarder et du contenu à mettre dedans
+TODO : voir si faut donner juste le nom ou l'arborescence ici
+*/
+void sauvgarde(std::string nom_fichier,std::string contenu){
+    std::fstream mon_fichier;
+    mon_fichier.open("nom_fichier", ios::out);
+}
